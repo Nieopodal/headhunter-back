76a9db6d5b4bb6589ee6faa06a3878e8d82999a5
Arkadiusz Bubelski <bubelarek@gmail.com>
2023-04-25 23:00:29 +0200

    parse and send back data from csv

--- src/init-student-data/init-student-data.module.ts
+++ src/init-student-data/init-student-data.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { InitStudentDataService } from './init-student-data.service';
 import { InitStudentDataController } from './init-student-data.controller';
-import { CsvModule } from 'nest-csv-parser';
 
 @Module({
-  imports: [CsvModule],
   controllers: [InitStudentDataController],
   providers: [InitStudentDataService]
 })
