03b5e1981b6241570e257cadd7ddf460bbaba388
WysoPC <mateusz.wyso94@gmail.com>
2023-04-25 09:51:56 +0200

    Enable Frontend-Backend Communication - CORS

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,17 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import cookieParser from "cookie-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
+  app.use(cookieParser());
   app.enableCors({
-    origin: 'http://localhost:3000', // [FE] port: 3000
+    origin: configService.get('CORS_CLIENTS_URL'), // [FE] port: 3001
     credentials: true,
   });
-  await app.listen(configService.get('PORT') ? parseInt(configService.get('PORT')) : 3001); // [BE] port: 3001
+  await app.listen(configService.get('PORT') ? parseInt(configService.get('PORT')) : 3000); // [BE] port: 3000
 }
 bootstrap();
