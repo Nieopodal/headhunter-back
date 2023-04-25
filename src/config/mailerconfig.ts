bd4b1b7a1909e48178642373fa1f2f0a20c5d679
WysoPC <mateusz.wyso94@gmail.com>
2023-04-25 15:57:54 +0200

    mailerconfig with .env

--- src/config/mailerconfig.ts
+++ src/config/mailerconfig.ts
@@ -1,20 +1,24 @@
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 import { ConfigService } from '@nestjs/config';
 
-export default (mailerConfigService: ConfigService) => {
-    const getEnv = (key: string) => mailerConfigService.get(key);
-
-    return {
-        transport: `smtp://${getEnv('EMAIL_USERNAME')}:${getEnv('EMAIL_PASSWORD')}@${getEnv('EMAIL_HOST')}:${getEnv('EMAIL_PORT')}`,
-        defaults: {
-            from: getEnv('EMAIL_FROM'),
+export default (configService: ConfigService) => ({
+    transport: {
+        host: configService.get('EMAIL_HOST'),
+        port: parseInt(configService.get('EMAIL_PORT')),
+        secure: configService.get('EMAIL_SECURE') === 'true',
+        auth: {
+            user: configService.get('EMAIL_USERNAME'),
+            pass: configService.get('EMAIL_PASSWORD'),
         },
-        template: {
-            dir: './templates/email',
-            adapter: new HandlebarsAdapter(),
-            options: {
-                strict: true,
-            },
+    },
+    defaults: {
+        from: configService.get('EMAIL_FROM'),
+    },
+    template: {
+        dir: './templates/email',
+        adapter: new HandlebarsAdapter(),
+        options: {
+            strict: true,
         },
-    };
-};
+    },
+});
