d656c1347778e36e93033195131285bcf1b968a0
Radosław Kowalski <ladekesl@gmail.com>
2023-04-26 20:18:46 +0200

    'Added first unit test for AuthService'

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,55 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { Test, TestingModule } from "@nestjs/testing";
+import { AuthService } from "./auth.service";
+import { AdminService } from "../admin/admin.service";
+import { StudentService } from "../student/student.service";
+import { HrService } from "../hr/hr.service";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
+import * as bcrypt from 'bcrypt';
+import { Student } from "../student/entity/student.entity";
 
-describe('AuthService', () => {
+describe("AuthService", () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [AuthService,
+        AdminService,
+        StudentService,
+        HrService,
+        JwtService,
+        ConfigService
+      ]
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
+  it("should be defined", () => {
     expect(service).toBeDefined();
+  });
+
+  describe("hashData", () => {
+    it("should return a hashed string", async () => {
+      const data = 'test123';
+      const hashedData = await service.hashData(data);
+      const isMatch = await bcrypt.compare(data, hashedData);
+      expect(isMatch).toBe(true);
+    });
+  });
+  describe('updateRtHash', () => {
+    it('should update the user refresh token hash', async () => {
+      const id = 1;
+      const rt = 'test123';
+      const mockUser: Partial<Student> = {
+        refreshToken: '',
+        save: jest.fn(),
+      };
+      jest.spyOn(service, 'checkUserById').mockResolvedValue(mockUser as Student);
+      await service.updateRtHash(id, rt);
+
+      expect(mockUser.refreshToken).not.toBe('');
+      expect(mockUser.save).toHaveBeenCalled();
+    });
   });
 });
--- src/hr/entity/hr.entity.ts
+++ src/hr/entity/hr.entity.ts
@@ -1,13 +1,13 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class Hr extends BaseEntity {
-  @PrimaryGeneratedColumn('uuid')
+  @PrimaryGeneratedColumn("uuid")
   id: string;
 
   @Column({
     unique: true,
-    length: 255,
+    length: 255
   })
   email: string;
 
@@ -17,20 +17,18 @@ export class Hr extends BaseEntity {
   @Column({ length: 255 })
   password: string;
 
-  @Column({ length: 255 })
-  fullName: string;
 
-  @Column({ default: 'hr', length: 20 })
+  @Column({ default: "hr", length: 20 })
   role: string;
 
   @Column({
-    length: 150,
+    length: 150
   })
   company: string;
 
   @Column({ default: 0 })
   maxReservedStudents: number;
 
-  @Column({ default: '', length: 255, nullable: true })
+  @Column({ default: "", length: 255, nullable: true })
   refreshToken: string;
 }
