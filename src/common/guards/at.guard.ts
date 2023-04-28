a729b23a5de51d0a8991d77c38ac7363c545ce23
WysoPC <mateusz.wyso94@gmail.com>
2023-04-28 08:56:13 +0200

    problem with server. Added 'rxjs' one more time

--- src/common/guards/at.guard.ts
+++ src/common/guards/at.guard.ts
@@ -1,6 +1,7 @@
 import { AuthGuard } from '@nestjs/passport';
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import {Observable} from "rxjs";
 
 @Injectable()
 export class AtGuard extends AuthGuard('jwt-access') {
@@ -8,7 +9,7 @@ export class AtGuard extends AuthGuard('jwt-access') {
     super();
   }
 
-  canActivate(context: ExecutionContext) {
+  canActivate(context: ExecutionContext): Observable<boolean> | Promise<boolean> | boolean {
     const isPublic = this.reflector.getAllAndOverride('isPublic', [context.getHandler(), context.getClass()]);
     if (isPublic) {
       return true;
