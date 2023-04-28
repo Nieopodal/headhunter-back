046d5c9129bf7f57cc8c31dde9385cd923664dee
WysoPC <mateusz.wyso94@gmail.com>
2023-04-28 10:17:43 +0200

    test host

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setGlobalPrefix('api');
+
   const configService = app.get(ConfigService);
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
