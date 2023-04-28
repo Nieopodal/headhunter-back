e49cd8458ce96b31882eff9e49a02ef46df144b9
Radosław Kowalski <ladekesl@gmail.com>
2023-04-28 18:57:39 +0200

    Add additional tests for auth module

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -7,6 +7,8 @@ import { JwtService } from "@nestjs/jwt";
 import { ConfigService } from "@nestjs/config";
 import * as bcrypt from "bcrypt";
 import { Student } from "../student/entity/student.entity";
+import { Admin } from "../admin/entity/admin.entity";
+import { Hr } from "../hr/entity/hr.entity";
 
 describe("AuthService", () => {
   let service: AuthService;
@@ -48,6 +50,7 @@ describe("AuthService", () => {
       expect(isMatch).toBe(true);
     });
   });
+
   describe("updateRtHash", () => {
     it("should update the user refresh token hash", async () => {
       const id = 1;
@@ -63,6 +66,7 @@ describe("AuthService", () => {
       expect(mockUser.save).toHaveBeenCalled();
     });
   });
+
   describe("getTokens", () => {
     it("should return a Tokens object with access_token and refresh_token", async () => {
       jest.spyOn(jwtService, "signAsync").mockImplementation(async () => "mock_token");
@@ -72,6 +76,7 @@ describe("AuthService", () => {
       expect(tokens.refresh_token).toBe("mock_token");
     });
   });
+
   describe("getDecodedToken", () => {
     it("should return decoded token", async () => {
       const mockToken = "mock_token";
@@ -100,8 +105,47 @@ describe("AuthService", () => {
 
       expect(result).toBeNull();
     });
+
+    it("should return the correct user for a given email", async () => {
+      const mockAdmin: Partial<Admin> = { id: "1", email: "admin@example.com" };
+      const mockHR: Partial<Hr> = { id: "2", email: "hr@example.com" };
+      const mockStudent: Partial<Student> = { id: "3", email: "student@example.com" };
+
+      jest.spyOn(adminService, "getUserByEmail").mockResolvedValue(mockAdmin as Admin);
+      jest.spyOn(hrService, "getUserByEmail").mockResolvedValue(mockHR as Hr);
+      jest.spyOn(studentService, "getUserByEmail").mockResolvedValue(mockStudent as Student);
+
+      const email = "hr@example.com";
+      const result = await service.checkUserByEmail(email);
+
+      expect(result).toEqual(mockHR);
+    });
+
+    describe("checkUserById", () => {
+      it("should return nullable if no user found", async () => {
+        jest.spyOn(adminService, "getUserById").mockResolvedValue(null);
+        jest.spyOn(hrService, "getUserById").mockResolvedValue(null);
+        jest.spyOn(studentService, "getUserById").mockResolvedValue(null);
+
+        const result = await service.checkUserById(null);
+
+        expect(result).toBeNull();
+      });
+
+      it("should return the correct user for a given id", async () => {
+        const mockAdmin: Partial<Admin> = { id: "1" };
+        const mockHR: Partial<Hr> = { id: "2" };
+        const mockStudent: Partial<Student> = { id: "3" };
+
+        jest.spyOn(adminService, "getUserById").mockResolvedValue(mockAdmin as Admin);
+        jest.spyOn(hrService, "getUserById").mockResolvedValue(mockHR as Hr);
+        jest.spyOn(studentService, "getUserById").mockResolvedValue(mockStudent as Student);
+
+        const id = "1";
+        const result = await service.checkUserById(id);
+
+        expect(result).toEqual(mockAdmin);
+      });
+    });
   });
 });
-
-
-
