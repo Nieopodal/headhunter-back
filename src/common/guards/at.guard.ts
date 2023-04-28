1cfbe6617644982200009ebe4d246466471cb8cd
WysoPC <mateusz.wyso94@gmail.com>
2023-04-28 08:53:12 +0200

    problem with server. Delete 'rxjs'

--- src/common/guards/at.guard.ts
+++ src/common/guards/at.guard.ts
@@ -1,7 +1,6 @@
 import { AuthGuard } from '@nestjs/passport';
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
 
 @Injectable()
 export class AtGuard extends AuthGuard('jwt-access') {
@@ -9,7 +8,7 @@ export class AtGuard extends AuthGuard('jwt-access') {
     super();
   }
 
-  canActivate(context: ExecutionContext): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(context: ExecutionContext) {
     const isPublic = this.reflector.getAllAndOverride('isPublic', [context.getHandler(), context.getClass()]);
     if (isPublic) {
       return true;
