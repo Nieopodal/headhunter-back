89eda4d8ac2ef5fc932a4e192cfd1ecab9d9e9cf
WysoPC <mateusz.wyso94@gmail.com>
2023-05-03 10:15:54 +0200

    test host

--- src/common/guards/at.guard.ts
+++ src/common/guards/at.guard.ts
@@ -1,18 +1,26 @@
-import { AuthGuard } from '@nestjs/passport';
-import { ExecutionContext, Injectable } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
+import {AuthGuard} from '@nestjs/passport';
+import {ExecutionContext, Injectable} from '@nestjs/common';
+import {Reflector} from '@nestjs/core';
+import {from, Observable, of} from "rxjs";
 
 @Injectable()
 export class AtGuard extends AuthGuard('jwt-access') {
-  constructor(private reflector: Reflector) {
-    super();
-  }
-
-  canActivate(context: ExecutionContext) {
-    const isPublic = this.reflector.getAllAndOverride('isPublic', [context.getHandler(), context.getClass()]);
-    if (isPublic) {
-      return true;
+    constructor(private reflector: Reflector) {
+        super();
     }
-    return super.canActivate(context);
-  }
+
+    canActivate(context: ExecutionContext): Observable<boolean> {
+        const isPublic = this.reflector.getAllAndOverride('isPublic', [context.getHandler(), context.getClass()]);
+        if (isPublic) {
+            return of(true);
+        }
+        const result = super.canActivate(context);
+        if (typeof result === 'boolean') {
+            return of(result);
+        } else if (result instanceof Promise) {
+            return from(result);
+        } else {
+            return result;
+        }
+    }
 }
