3a29710f523061f51d6d5e4d453ac10022a97387
Wojtek <rossa21@gmail.com>
2023-05-05 23:02:51 +0200

    Added provider in hr module.

--- src/hr/hr.module.ts
+++ src/hr/hr.module.ts
@@ -7,11 +7,12 @@ import { AdminService } from '../admin/admin.service';
 import { StudentService } from '../student/student.service';
 import { JwtService } from '@nestjs/jwt';
 import { StudentModule } from '../student/student.module';
+import { StudentHrService } from '../student/student-hr.service';
 
 @Module({
   imports: [forwardRef(() => AuthModule), forwardRef(() => StudentModule)],
   controllers: [HrController],
-  providers: [HrService, AuthService, AdminService, StudentService, JwtService],
+  providers: [HrService, AuthService, AdminService, StudentHrService, StudentService, JwtService],
   exports: [HrService],
 })
 export class HrModule {}
