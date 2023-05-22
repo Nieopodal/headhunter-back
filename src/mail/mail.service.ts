3d069197e6c259aabf29a93ec7b93743cdedb860
IrePro78 <gos3r78@gmail.com>
2023-05-22 14:53:21 +0200

    added docker-compose context

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -17,7 +17,7 @@ export class MailService {
   async generateUrl(data, actionType): Promise<string> {
     const { id, role, verificationToken } = data;
     const appUrl = this.configService.get('APP_URL');
-    return `${appUrl}/api/auth/${actionType}/${role}/confirm/${id}/${verificationToken}`;
+    return `${appUrl}/auth/${actionType}/${role}/confirm/${id}/${verificationToken}`;
   }
 
   async sendMail(to: string, subject: string, html: string): Promise<SendMailInfo> {
